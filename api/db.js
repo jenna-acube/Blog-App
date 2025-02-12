b947b27ce19dfc9d9085e8af85ec1f0ede081f3b
unknown <jenna@acubetechnologies.in>
2025-02-12 11:01:34 +0530

    Changes

--- api/db.js
+++ api/db.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: 'sql303.infinityfree.com',
   user: process.env.DB_USER,
   password: process.env.DB_PASS ,
   database: process.env.DB_NAME,
